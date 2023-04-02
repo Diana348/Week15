eb45ba844a0e7a1958cbf487c222e466d78d7db6
Diana348 <popovad30@gmail.com>
2023-04-03 01:10:07 +0700

    question

---
+++ assets/scripts/index.js
@@ -0,0 +1,11 @@
+let list = document.querySelector('.todo-list');
+let input = document.querySelector('.todo-input');
+let form = document.querySelector('.todo-form');
+
+form.onsubmit = function (evt) {
+    evt.preventDefault();
+    let newTask = document.createElement('li');
+    newTask.textContent = input.value;
+    list.append(newTask);
+    input.value = '';
+};
